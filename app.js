47e0abc00cce8309ce646d83b42fe937381f829d
Michael Yeates <michael-yeates@outlook.com>
2023-03-17 00:19:02 +0000

    Update port

--- app.js
+++ app.js
@@ -131,6 +131,6 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
 });
